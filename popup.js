f2d68a106884d55ac8399e842dcd802744c25c40
Lim Daekoon <daekoon_lim@hotmail.com>
2021-01-09 11:17:20 +0800

    Complete version 1.0

--- popup.js
+++ popup.js
@@ -1,10 +1,37 @@
-let changeColor = document.getElementById('enable');
+let adButton = document.getElementById('enable');
+let globalButton = document.getElementById('kek');
 
-changeColor.onclick = function(element) {
-  let color = element.target.value;
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+function hello() {
+  chrome.extension.getBackgroundPage().console.log("Turning background color red!")
+  chrome.tabs.executeScript(
+    null,
+    {file: 'filter.js'},
     chrome.tabs.executeScript(
-        tabs[0].id,
-        {code: 'document.body.style.backgroundColor = "red";'});
-  });
-};
+      null,
+      {file: 'matter_minified.js'},
+      chrome.tabs.executeScript(
+        null,
+        {file: 'working.js'}
+      )
+    )
+  )
+}
+
+function hello2() {
+  chrome.extension.getBackgroundPage().console.log("Turning background color red!")
+  chrome.tabs.executeScript(
+    null,
+    {file: 'filter.js'},
+    chrome.tabs.executeScript(
+      null,
+      {file: 'matter_minified.js'},
+      chrome.tabs.executeScript(
+        null,
+        {file: 'workingcopy.js'}
+      )
+    )
+  )
+}
+
+adButton.addEventListener('click', hello);
+globalButton.addEventListener('click', hello2);
